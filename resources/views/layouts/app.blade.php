e18481ad9c534fcd428e2a8f71360c2a182fc5b4
Jobish <jobishwork@gmail.com>
2018-03-24 10:26:11 +0530

    change on app.blade

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,12 +13,7 @@
       <nav class="navbar navbar-default navbar-fixed-top">
          <div class="container">
             <div class="navbar-header">
-               <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
-               <span class="sr-only">Toggle Navigation</span>
-               <span class="icon-bar"></span>
-               <span class="icon-bar"></span>
-               <span class="icon-bar"></span>
-               </button>
+
                <a class="navbar-brand" href="{{ url('/') }}">
                   {{ config('app.name', 'Laravel') }}
                </a>
