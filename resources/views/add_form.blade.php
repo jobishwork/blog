04ce407c5f3807bc2723753bc32152e9e0a25eaa
Joyal James <joyaljames@gmail.com>
2018-03-29 16:24:00 +0530

    tinymce added

--- resources/views/add_form.blade.php
+++ resources/views/add_form.blade.php
@@ -1,7 +1,5 @@
 @extends('layouts.app')
 @section('content')
-<script src="https://cloud.tinymce.com/stable/tinymce.min.js"></script>
-  <script>tinymce.init({ selector:'textarea', branding:false });</script>
 <div class="container">
    <div class="row">
       <!-- Blog Post Content Column -->
